ff62fc73299796c04de93c0443ef2372a69c688d
rocky <fengjian0213@126.com>
2016-02-18 17:52:28 +0800

    creat

--- JPushTest/AppDelegate.m
+++ JPushTest/AppDelegate.m
@@ -7,9 +7,10 @@
 //
 
 #import "AppDelegate.h"
-
+#import "JPUSHService.h"
+#import "ViewController.h"
 @interface AppDelegate ()
-
+@property (nonatomic, strong) UILabel *notification;
 @end
 
 @implementation AppDelegate
@@ -17,8 +18,85 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // Required
+    
+    
+    
+    self.notification = [[UILabel alloc]initWithFrame:CGRectMake(50, 50, 250, 30)];
+    self.notification.textColor = [UIColor blackColor];
+    self.notification.backgroundColor = [UIColor grayColor];
+    [self.window addSubview:_notification];
+    
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        //可以添加自定义categories
+        [JPUSHService registerForRemoteNotificationTypes:(UIUserNotificationTypeBadge |
+                                                          UIUserNotificationTypeSound |
+                                                          UIUserNotificationTypeAlert)
+                                              categories:nil];
+    } else {
+        //categories 必须为nil
+        [JPUSHService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                          UIRemoteNotificationTypeSound |
+                                                          UIRemoteNotificationTypeAlert)
+                                              categories:nil];
+    }
+    
+    // Required
+    //如需兼容旧版本的方式，请依旧使用[JPUSHService setupWithOption:launchOptions]方式初始化和同时使用pushConfig.plist文件声明appKey等配置内容。
+    [JPUSHService setupWithOption:launchOptions appKey:@"b542dc974a9592f02232ada1" channel:nil apsForProduction:NO];
+    
+
+    // 获取推送过来的消息
+    // 如果 App 状态为未运行，此函数将被调用
+    NSDictionary *remoteNotification = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    NSLog(@"remoteNotification %@",remoteNotification);
+   
+    
     return YES;
 }
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    // Required
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+    // Required,For systems with less than or equal to iOS6
+    [JPUSHService handleRemoteNotification:userInfo];
+    
+    
+   
+
+}
+// 如果 App状态为正在前台或者点击通知栏的通知消息，那么此函数将被调用
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // IOS 7 Support Required
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+    
+     NSLog(@"userInfo : %@",userInfo);
+    // 取得 APNs 标准信息内容
+    NSDictionary *aps = [userInfo valueForKey:@"aps"];
+    NSString *content = [aps valueForKey:@"alert"]; //推送显示的内容
+    NSInteger badge = [[aps valueForKey:@"badge"] integerValue]; //badge数量
+    NSString *sound = [aps valueForKey:@"sound"]; //播放的声音
+    
+    // 取得Extras字段内容
+    NSString *customizeField1 = [userInfo valueForKey:@"customizeExtras"]; //服务端中Extras字段，key是自己定义的
+    NSLog(@"content =[%@], badge=[%d], sound=[%@], customize field  =[%@]",content,badge,sound,customizeField1);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    
+    //Optional
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -28,10 +106,13 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[UIApplication sharedApplication]setApplicationIconBadgeNumber:0];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [application setApplicationIconBadgeNumber:0];
+    [application cancelAllLocalNotifications];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
--- JPushTest/ViewController.m
+++ JPushTest/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import "JPUSHService.h"
 @interface ViewController ()
-
+@property (nonatomic, strong) UILabel *notification;
 @end
 
 @implementation ViewController
@@ -17,11 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 点击清除服务器角标
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clearBadge:)];
+    [self.view addGestureRecognizer:tap];
+    
+    
+    // 获取自定义消息推送内容
+    // 只有在前端运行的时候才能收到自定义消息的推送
+    NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+    [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kJPFNetworkDidReceiveMessageNotification object:nil];
+    
+    
+    self.notification = [[UILabel alloc]initWithFrame:CGRectMake(50, 50, 250, 30)];
+    self.notification.textColor = [UIColor blackColor];
+    self.notification.backgroundColor = [UIColor grayColor];
+    self.notification.text = self.noti;
+    [self.view addSubview:_notification];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 获取自定义消息回调方法
+- (void)networkDidReceiveMessage:(NSNotification *)notification{
+    NSDictionary * userInfo = [notification userInfo];
+    //    NSString *content = [userInfo valueForKey:@"content"];
+    //    NSDictionary *extras = [userInfo valueForKey:@"extras"];
+    //    NSString *customizeField1 = [extras valueForKey:@"customizeField1"]; //自定义参数，key是自己定义的
+    NSLog(@"自定义消息 %@",userInfo);
+    
+    self.notification.text = [userInfo valueForKey:@"content"];
+}
+
+
+- (void)clearBadge:(UITapGestureRecognizer *)tap{
+//    [JPUSHService setBadge:0];
+    [JPUSHService resetBadge];
 }
 
 @end
